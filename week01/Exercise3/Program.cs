580796ec0b35c39164b704113afb9335ba74a527
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-10 23:41:07 -0600

    `Refactor guess logic in Program.cs to use a while loop`

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -13,18 +13,22 @@ class Program
         int guessNumber = int.Parse(guess);
 
         
-        if (guessNumber < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (guessNumber > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+        
 
+        while (guessNumber != magicNumber)
+        {
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
