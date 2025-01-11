0db5b2978a2e5dbbfea2d4398b77c8cabf624ab1
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-11 12:51:46 -0600

    "Added input validation and number

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -9,13 +9,20 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers  = new List<int>();
-        
+
         int number = -1;
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
         while (number != 0)
         {
             Console.Write("Enter a number: ");
             number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
         }
     }
 }
