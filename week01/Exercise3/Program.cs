b72984fc1bcd1ca2fc4bd18404834822eee6267c
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-10 23:22:03 -0600

    Writing the challenges using If Statements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,8 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number? ");
+        Console.Write("What is the magic number? ");
         string input = Console.ReadLine();
         int magicNumber  = int.Parse(input);
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessNumber = int.Parse(guess);
+
+        
+        if (guessNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
