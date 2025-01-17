8c2b49b65bb7477dc98f87bf3e24d111d7362a7e
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-17 16:36:17 -0600

    Created an Instance of the Job Class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
 }
