a55225c76e3dfbebe360b10acc7ceac8ababdb18
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-17 16:47:20 -0600

    Updated the class to contain the member methods

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,16 +1,19 @@
 public class Job
-{
+{   
+    // member variables(attributes) 
     public string _company = "";
     public string _jobTitle = "";
     public int _startYear = -1;
     public int _endYear = -1;
 
+    // A Constructor is made for the Job class
     public Job()
     {
     }
 
-    // public void Display()
-    // {
-    //     _compan
-    // }
+    // member methods (functions)
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
 }
