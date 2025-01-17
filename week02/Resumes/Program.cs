abc03e23c9835d43634d6ece381ac1d0e8fa6eca
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-17 16:48:02 -0600

    Updated the Program to call its method

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,11 +6,17 @@ class Program
     {
         Job job1 = new Job();
         job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        Console.WriteLine(job1._company);
-        Console.WriteLine(job2._company);
+        job1.Display();
+        job2.Display();
     }
 }
