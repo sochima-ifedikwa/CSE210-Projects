0e47319f7df856040b7838ccbfb4401220072bdb
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-17 16:35:51 -0600

    Edited the job class to contain the right member variables

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,7 +1,16 @@
 public class Job
 {
-    public string _name = "";
-    public List<Job> _jobs = new List<Job>();
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear = -1;
+    public int _endYear = -1;
 
-    
+    public Job()
+    {
+    }
+
+    // public void Display()
+    // {
+    //     _compan
+    // }
 }
