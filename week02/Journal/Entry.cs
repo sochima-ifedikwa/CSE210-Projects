c58734691df9a1c80677b0b73a1ac2d019098581
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-20 16:09:26 -0600

    "Removed unused import and added simple menu-driven program to Entry.cs and Program.cs respectively."

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,6 +1,4 @@
-using System.Security.Cryptography.X509Certificates;
-
-public class Entry
+ public class Entry
 {
     public string _date = "";
     public string _promptText = "";
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("Please select the following choices");
+        int choice = -1;
+        while (choice != 5)
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter a word: ");
+                string word = Console.ReadLine();
+                Console.WriteLine("You entered: " + word);
+            }
+        }
+
     }
 }
