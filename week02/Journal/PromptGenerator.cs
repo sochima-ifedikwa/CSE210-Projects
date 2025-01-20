ce4ed5dc87840be7a28aeb409d680f48cae1550f
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-19 18:48:36 -0600

    Added a list of Prompts and the functionality to pick a prompt at random based on the amount of promts in the list

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,10 +1,18 @@
 public class PromptGenerator
 {
-    public List<string> = new List<string>();
+    public List<string> _prompts = new List<string>()
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the most challenging thing I faced today?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the most joyful moment I experienced today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
 
     public string GetRandomPrompt()
     {
-        // Return a random prompt from the list
-        
+        Random random = new Random();
+        int number = random.Next(_prompts.Count);
+        return _prompts[number];
     }
 }
