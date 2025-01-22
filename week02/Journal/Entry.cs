eec3e21b2a7bca3551c4c14bb34aeec1905bb883
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-21 21:12:42 -0600

    "Added using directive, properties, and commented out constructor to Entry.cs. Added PromptGenerator instance to Program.cs."

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,12 +1,21 @@
- public class Entry
+using System;
+public class Entry
 {
-    public string _date = "";
-    public string _promptText = "";
-    public string _entryText = "" ;
+    public string _date;
+    public string _promptText;
+    public string _entryText;
 
+    // public Entry(string date, string promptText, string entryText);
+    // {
+    //     _date = date;
+    //     _promptText = promptText;
+    //     _entryText = entryText;
+    // }
     public void Display()
     {
-        
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_promptText}");
+        Console.WriteLine($"Entry: {_entryText}");
     }
 
 }
--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,21 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
 
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
     public void DisplayAll()
     {
-
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
     public void SaveTofile(string file)
     {
+        Console.Write("What is the filename?");
+        string filename = Console.ReadLine();
 
+        using (StreamWriter writer = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine($"Date: {entry._date} - Prompt: {entry._promptText}{entry._entryText}");
+            }
+        }
     }
 
     public void LoadFromFile(string file)
-    {}
+    {
+        if (File.Exists(file))
+        {
+            _entries.Clear();
+            string[] lines = File.ReadAllLines(file);
+            foreach(var line in lines)
+            {
+                string[] parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    Entry entry = new Entry
+                    {
+                        _date = parts[0],
+                        _promptText = parts [1],
+                        _entryText = parts [2]
+                    };
+                    _entries.Add(entry);
+                }
+            }
+        
+        }
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+     }
 }
