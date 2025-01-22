7a058447b2c3e946a278cd92c581753e7577eb1f
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-21 21:12:58 -0600

    Finished the Journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -8,6 +8,7 @@ class Program
         Journal theJournal = new Journal(); // Create a new instance of the Journal class
         Entry anEntry = new Entry(); // Create a new instance of the Entry class
         
+        
 
         Console.WriteLine("Welcome to the program!");
         Console.WriteLine("Please select the following choices");
@@ -23,7 +24,16 @@ class Program
             choice = int.Parse(Console.ReadLine());
             if (choice == 1)
             {
-                theJournal.AddEntry();
+                Entry entry = new Entry();
+                DateTime theCurrentDate = DateTime.Now;
+                entry._date = theCurrentDate.ToShortDateString();
+                PromptGenerator promptGenerator = new PromptGenerator();
+                string prompt = promptGenerator.GetRandomPrompt();
+                entry._promptText = prompt;
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                entry._entryText = Console.ReadLine();
+                theJournal.AddEntry(entry);
             }
             else if (choice == 2)
             {
@@ -31,11 +41,19 @@ class Program
             }
             else if (choice == 3)
             {
-                theJournal.LoadFromFile();
+                Console.Write("What is the file name? ");
+                string file = Console.ReadLine();
+                theJournal.LoadFromFile(file);
+                Console.WriteLine("Success:  journal loaded!");
+                
             }
             else if (choice == 4)
             {
-                theJournal.SaveTofile();
+                Console.Write("What is the file name? ");
+                string file = Console.ReadLine();
+                theJournal.SaveTofile(file);
+                Console.WriteLine("Success:  journal saved!");
+                
             }
         }
 
