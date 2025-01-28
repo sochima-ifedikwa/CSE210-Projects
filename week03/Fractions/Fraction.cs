98b658a6bd69885354c5af0d3b7c906be96ad077
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-28 04:02:48 -0600

    Added constructor parameter initialization in Fraction class

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,23 @@
+public class Fraction {
+    private int _top;
+    private int _bottom;
+
+    public Fraction() 
+    {
+        _top = 1;
+        _bottom = 1;
+        
+    }
+    public Fraction(int WholeNumber)
+    {
+        _top = WholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+        
+    }
+}
