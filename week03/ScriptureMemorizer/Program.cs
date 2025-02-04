9d45f33dce54b9a1be649dd50d79e49843e4cc54
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-04 16:35:09 -0600

    Added Scripture memorization functionality to the program, including hiding random words and checking for complete memorization.`

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference = null;
+        Scripture scripture = null;
+
+        while(true) {
+            Console.Clear();
+            Console.Write($"{reference.GetDisplayText()}");
+            Console.WriteLine($"{scripture.GetDisplayText()}");
+
+            Console.Write("Press Enter to continue or type 'quit' to finish:");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "quit") {
+                break;
+            }
+            else if (userInput == "") {
+                // do nothing
+                scripture.HideRandomWords(2);
+
+                if (scripture.IsCompletelyHidden()) {
+                    Console.WriteLine("You have fully memorized the scripture! Congratulations!");
+                    break;
+                }
+            }
+        }
     }
 }
