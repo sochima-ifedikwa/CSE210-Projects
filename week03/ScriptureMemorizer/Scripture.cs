5c9122a2aba83229653b846004ee2f19f1cf8889
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-04 16:35:18 -0600

    `Refactor Scripture class: add functionality to hide random words and improve display text generation`

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -10,19 +10,40 @@ public class Scripture {
         string[] words = text.Split(' ');
         foreach (string word in words) {
             _words.Add(new Word(word));
-            }
+        }
     }
     public void HideRandomWords(int NumberToHide) 
     {
-        return ;
+        Random random = new Random();
+        int x = 0;
+        int index;
+        while (x < NumberToHide) {
+            index = random.Next(0, _words.Count);
+            if (!_words[index].IsHidden()) {
+                _words[index].Hide();
+                x++;
+            }
+        }
     }
 
     public string GetDisplayText() 
     {
-        return "Scripture";
+        List<string> scriptureWords = new List<string>();
+        foreach (Word word in _words) 
+        {
+            scriptureWords.Add(word.GetDisplayText());
+        }
+        return string.Join(" ", scriptureWords);
     }
 
-    public bool IsCompletelyHidden() {
+    public bool IsCompletelyHidden() 
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden()) {
+                return false;
+            }
+        }
         return true;
     }
 }
