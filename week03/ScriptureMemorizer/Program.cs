7c8c95135c2042e5b7956226b68113a44207f58c
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-05 17:24:57 -0600

    Added user choice for short or long scripture references and implemented corresponding logic.`

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,28 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.WriteLine("Hello World! This is the Scripture Memorizer Project.");
 
         Reference reference = null;
         Scripture scripture = null;
 
-        while(true) {
+        Console.WriteLine("Do you want a short scripture reference or a long one ? (long/short)");
+        string userChoice = Console.ReadLine();
+
+        //creating an instance of a short passage
+        Reference shortReference = new Reference("Proverbs", 3, 5);
+        Scripture shortScripture = new Scripture(shortReference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding.");
+
+        // creating an instance of a long passage
+        Reference longReference = new Reference("Proverbs", 3, 5, 6);
+        Scripture longScripture = new Scripture(longReference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+
+        if (userChoice.ToLower() == "short") {
+            reference = shortReference;
+            scripture = shortScripture;
+        }
+        else if (userChoice.ToLower() == "long") {
+            reference = longReference;
+            scripture = longScripture;
+        }
+
+        while (true) {
             Console.Clear();
-            Console.Write($"{reference.GetDisplayText()}");
+            Console.WriteLine($"{reference.GetDisplayText()}");
             Console.WriteLine($"{scripture.GetDisplayText()}");
 
-            Console.Write("Press Enter to continue or type 'quit' to finish:");
+            Console.Write("Press enter to continue or type 'q' to quit: ");
             string userInput = Console.ReadLine();
 
-            if (userInput == "quit") {
+            if (userInput.ToLower() == "q") {
                 break;
             }
-            else if (userInput == "") {
-                // do nothing
+            else if(userInput == "") {
                 scripture.HideRandomWords(2);
 
                 if (scripture.IsCompletelyHidden()) {
-                    Console.WriteLine("You have fully memorized the scripture! Congratulations!");
+                    Console.WriteLine("You have hidden all the words!");
                     break;
                 }
             }
