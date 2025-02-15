21e597afdd6298f322fe8decb5d1f17d0005bb7a
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-15 17:23:35 -0600

    Compiled Main Program to output the classes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine();
+        
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Reodriguez", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
