678ccbb579b666418c87239c6ac8058e6dd5b898
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-15 17:23:08 -0600

    Created the base class to include getters and setters instead of a Private

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -3,10 +3,31 @@ public class Assignment
     private string _studentName;
     private string _topic;
 
+    
     public Assignment(string studentName, string topic)
     {
         _studentName = studentName;
         _topic = topic;
+    }
+
+    public void SetStudentName(string studentName)
+    {
+        _studentName = studentName;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public void SetTopic(string topic)
+    {
+        _topic = topic;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
     }
     public string GetSummary()
     {
