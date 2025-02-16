4a901dbdde9d5781f53e26ec34f422e4b5d3f995
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-16 16:50:52 -0600

    `Added implementation for BreathingActivity class with Run method and DisplayBreathingVisualizer helper method.`

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,4 +1,43 @@
-public class BreathingActivity
+public class BreathingActivity : Activity
 {
-    
+    public BreathingActivity(string name, string description, int duration) : base(name, description, duration)
+    {
+
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        int elapsed = 0;
+        Console.WriteLine("\n");
+        while (elapsed < _duration)
+        {
+            DisplayBreathingVisualizer("Breathe in...", 3, false);
+            //ShowCountDown(3);
+            DisplayBreathingVisualizer("Now Breathe out...", 3, true);
+            //ShowCountDown(3);
+            Console.WriteLine("");
+            elapsed += 6;
+        }
+        DisplayEndingMessage();
+    }
+
+    private void DisplayBreathingVisualizer(string message, int duration, bool isInhaling)
+    {
+        int maxArrows = 2 * duration;
+        for (int i = 1; i <= duration; i++)
+        {
+            int arrows;
+            if (isInhaling)
+                arrows = 2 * i; 
+            else
+                arrows = 2 * (duration - i + 1);  
+
+            string arrowText = new string(isInhaling ? '>' : '<', arrows);
+            string paddedText = arrowText.PadRight(maxArrows);
+            Console.Write($"\r{message} [{paddedText}]");  
+            Thread.Sleep(1000); 
+        }
+        Console.WriteLine();  
+    }
 }
