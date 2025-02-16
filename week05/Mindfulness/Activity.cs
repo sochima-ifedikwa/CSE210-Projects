02cfab560625c060b997c570ab2a175bcc1fb4ce
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-15 18:11:52 -0600

    `Added properties and constructor to Activity class`

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,4 +1,33 @@
 public class Activity
 {
-    
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    protected Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        
+    }
 }
