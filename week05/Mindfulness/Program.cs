c579b987ae4015c03b677bbbdaa02a8c919d8b5c
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-16 17:06:58 -0600

    Added infinite loop to Program.cs for menu-driven activities`

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,64 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
-    }
+
+         while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from menu: ");
+
+            string choice = Console.ReadLine();
+
+
+
+            List<string> reflectingPrompts = [
+                "Think of a recent moment when you were hard on yourself.",
+                "Think of a time when you did something really difficult.",
+                "Think of a time when you did something for yourself that made you happy.",
+                "Think of one small thing you appreciated today."
+            ];
+
+            List<string> reflectingQuestions = [
+                "Why was this experience meaningful to you?",
+                "Have you ever done anything like this before?",
+                "How did you get started?",
+                "How did you feel when it was complete?",
+                "What did you learn about yourself?",
+                "How can you apply this experience in the future?"
+            ];
+
+
+            List<string> listingPrompts = [
+                "Who are people that you appreciate?",
+                "What are personal strengths of yours?",
+                "Who are people that you have helped this week?",
+                "Who are some of your personal heroes?"
+            ];
+
+            switch (choice)
+            {
+                case "1":
+                    new BreathingActivity("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.", 30).Run();
+                    break;
+                case "2":
+                    new ReflectingActivity("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.", 30, reflectingPrompts, reflectingQuestions).Run();
+                    break;
+                case "3":
+                    new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.", 30,listingPrompts).Run();
+                    break;
+                case "4":
+                    Console.WriteLine("Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice, try again.");
+                    Thread.Sleep(2000);
+                    break;
+            }
+        }
+    }   
 }
