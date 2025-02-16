776958535420979c8ddd50d01fe76ed0831d8c05
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-16 16:49:28 -0600

    Refactor Activity class to include session duration input and countdown display

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -13,21 +13,48 @@ public class Activity
 
     public void DisplayStartingMessage()
     {
-
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name} \n");
+        Console.WriteLine(_description);
+        Console.Write("\nHow long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("\n\n");
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
     }
 
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(3);
+        Console.WriteLine($"You have completed {_duration} seconds of the {_name}.");
+        ShowSpinner(3);
     }
 
     public void ShowSpinner(int seconds)
     {
+        char[] spinnerChars = { '/', '-', '\\', '|' };
+        int counter = 0;
 
+        for (int i = 0; i < seconds * 4; i++)
+        {
+            Console.Write($"{spinnerChars[counter % 4]}");
+            counter++;
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+
+        }
+        Console.WriteLine();
     }
 
     public void ShowCountDown(int seconds)
     {
-        
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write($"{i}");
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+        Console.WriteLine();
     }
 }
