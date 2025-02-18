bd151052303171617cc2aa4c529bf8e81596ab02
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-18 12:38:02 -0600

    Created the oulines of the GoalManager class

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,45 @@
+public class GoalManager
+{
+    private List<Goal> _goals = new List<Goal>();
+    private int _score;
+
+    public GoalManager(){
+        _score = 0;
+        _goals = new();
+
+    
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo(){
+
+    }
+
+    public void ListGoalNames(){
+
+    }
+    public void ListGoalDetails(){
+
+    }
+
+    public void CreateGoal(){
+
+    }
+
+    public void RecordEvent(){
+
+    }
+
+    public void SaveGoals(){
+
+    }
+
+    public void LoadGoals(){
+
+    }
+    
+}
