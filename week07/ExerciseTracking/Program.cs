e7e3a0d9ec84299544c3d423bb1dfa3c0a49ff53
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-02-19 02:42:09 -0600

    implemeted the classes into the main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        List<Activity> activities = new();
+
+        Running running = new("03 Nov 2022", 30, 3);
+        activities.Add(running);
+
+        Cycling cycling = new("05 Oct 2024", 30, 10);
+        activities.Add(cycling);
+
+        Swimming swimming = new("15 Dec 2024", 20, 15);
+        activities.Add(swimming);
+        
+        foreach(Activity activity in activities){
+            Console.WriteLine(activity.GetSummary());
+        }
     }
+        
 }
